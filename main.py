ca5fd81faf83bd0b354d134728603148601cd621
ElizabethCreature <hannahjohnson5658@gmail.com>
2023-10-20 21:52:54 -0500

    Takes directory inside current folder and outputs created by dates for all images inside.

--- main.py
+++ main.py
@@ -1,15 +1,32 @@
 from PIL import Image
 from PIL.ExifTags import TAGS
+import os.path, time
+import datetime
+import pathlib
+import filetype
 
 
-image = Image.open("Clara.jpg")
-exifdata = image.getexif()
+def check_type(f):
+    if filetype.is_image(f):
+        return True
+    else:
+        return False
 
-if exifdata:
-    for tagid in exifdata:
-        tagname = TAGS.get(tagid, tagid)
-        value = exifdata.get(tagid)
-        print(f"{tagname}: {value}")
-else:
-    print("No EXIF data found in this image.")
+def get_date(f):
+    img = Image.open(f)
+    exifdata = img._getexif()
+    if exifdata:
+        date = exifdata.get(306)
+        return date
+    else:
+        file = pathlib.Path(f)
+        ctime = datetime.datetime.fromtimestamp(file.stat().st_ctime, tz=datetime.timezone.utc)
+        return ctime 
+
+directory = input('Enter directory to date:')
+for filename in os.listdir(directory):
+    f = os.path.join(directory, filename)
+    print(f"Image name {filename}, created at {get_date(f)}")
+    
+
 
